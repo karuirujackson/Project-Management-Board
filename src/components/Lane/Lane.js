671ecc6c81667b762d80389a6c4cd80b16298089
karuirujackson <jkjackaruiru641@gmail.com>
2024-07-02 01:40:51 +0300

    Styled-components in App.js, Lane.js and Task.js

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,14 +1,36 @@
+// import './Lane.css';
+import styled from 'styled-components';
 import Task from '../Task/Task';
-import './Lane.css';
+
+const LaneWrapper = styled.div`
+    text-align: left;
+    padding: 0;
+    background: lightGray;
+    border-radius: 20px;
+    min-height: 50vh;
+    width: 20vw;
+    
+    @media (max-width: 768px) {
+      margin-bottom: 5%;
+    }
+ `; 
+  const TitleStyle = styled.h2`
+    width: 100%;
+    padding-bottom: 10px;
+    text-align: center;
+    border-bottom: 1px solid darkGray;
+  `;
+
 
 function Lane({ laneId, title, loading, error,tasks, onDragStart, onDragOver, onDrop }) {
   return (
-    <div 
+    <LaneWrapper 
         className='Lane-wrapper' 
         onDragOver={onDragOver}
         onDrop={(e) => onDrop(e, laneId)}
       >
       <h2>{title}</h2>
+      <TitleStyle>{title}</TitleStyle>
       {
         loading || error ? (
           <span>
@@ -28,7 +50,7 @@ function Lane({ laneId, title, loading, error,tasks, onDragStart, onDragOver, on
           )
         )
       }
-    </div>
+    </LaneWrapper>
   );
 }
 
